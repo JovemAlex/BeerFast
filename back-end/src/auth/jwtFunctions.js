ef2223115d7e795cf574df90b9009ef2c70891f5
Aline Valadao <valadao.aline@gmail.com>
2023-02-28 22:19:15 +0100

    feat: login endpoint

---
+++ back-end/src/auth/jwtFunctions.js
@@ -0,0 +1,33 @@
+const jwt = require('jsonwebtoken');
+
+const fs = require('fs');
+    const path = require('path');
+    
+    const filePath = path.join(__dirname, '../../jwt.evaluation.key');
+
+const getSecret = () => fs.readFileSync(filePath, { encoding: 'utf-8' });
+
+const jwtConfig = {
+  algorithm: 'HS256',
+  expiresIn: '1d',
+};
+
+const createToken = (email) => {
+  const payload = { email };
+  const token = jwt.sign(payload, getSecret(), jwtConfig);
+  return token;
+};
+
+const authenticateToken = (token) => {
+  try {
+      const validateToken = jwt.verify(token, getSecret());
+      return validateToken;
+  } catch (err) {
+      return false;
+  }
+};
+
+module.exports = {
+  createToken,
+  authenticateToken,
+};
---
+++ back-end/src/controllers/loginController.js
@@ -0,0 +1,23 @@
+const loginService = require('../services/loginService');
+
+const login = async (req, res, next) => {
+  try {
+    const auth = await loginService.authenticateToken(req.body);
+    if (auth.type === 'USER_NOT_FOUND') {
+      return res
+        .status(404)
+        .json({ message: 'Not found' });
+    }
+
+    req.user = auth;
+
+    return res.status(200).json({ token: auth });
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
+
+module.exports = {
+  login,
+};
