6fb88dff62c2f73742e5f1b94552abd6805cfc8a
Miguel Inácio <miguel.angelo.inacio@gmail.com>
2023-02-28 17:42:46 -0300

    muda retorno do cadastro

--- back-end/src/services/registerService.js
+++ back-end/src/services/registerService.js
@@ -4,7 +4,12 @@ const { User } = require('../database/models');
 const register = async (data) => {
   const { name, email, password, role } = data;
   const hashedPassword = md5(password);
-  const user = await User.create({ name, email, password: hashedPassword, role });
+  const result = await User.create({ name, email, password: hashedPassword, role });
+  const user = {
+    name: result.name,
+    email: result.email,
+    role: result.role
+  }
   return user;
 };
 
