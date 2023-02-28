83f1b1dbbf5efc8eed95bc923ae9216db356eab4
Miguel Inácio <miguel.angelo.inacio@gmail.com>
2023-02-28 17:14:21 -0300

    adiciona hash de senha ao service

--- back-end/src/services/registerService.js
+++ back-end/src/services/registerService.js
@@ -1,8 +1,10 @@
 const { User } = require('../database/models');
+const md5 = require('md5');
 
 const register = async (data) => {
   const { name, email, password, role } = data;
-  const user = await User.create({ name, email, password, role });
+  const hashedPassword = md5(password);
+  const user = await User.create({ name, email, password: hashedPassword, role });
   return user;
 };
 
