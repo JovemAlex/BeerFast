5bbf8bbe6b7762ce04b5b2bc6ed929c30f67e23d
Miguel Inácio <miguel.angelo.inacio@gmail.com>
2023-02-28 14:45:37 -0300

    adiciona rota de register app

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+import registerRoutes from '../routes/register.routes'
 
 const app = express();
+app.use(express.json());
 
 app.get('/coffee', (_req, res) => res.status(418).end());
+
+app.use('/register', registerRoutes)
 
 module.exports = app;
