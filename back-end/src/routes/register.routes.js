f37bb65a006b222da41dc97403c77b267b7805c2
Miguel Inácio <miguel.angelo.inacio@gmail.com>
2023-02-28 17:35:16 -0300

    adiciona verificação para nome e senha

--- back-end/src/routes/register.routes.js
+++ back-end/src/routes/register.routes.js
@@ -1,9 +1,16 @@
 const { Router } = require('express');
 const registerController = require('../controllers/registerController');
 const { verifyFields } = require('../middlewares/verifyFields');
+const { verifyName, verifyPassword } = require('../middlewares/verifyLength');
 
 const register = Router();
 
-register.post('/', verifyFields, (req, res) => registerController.register(req, res));
+register.post(
+  '/',
+  verifyFields,
+  verifyName, 
+  verifyPassword, 
+  (req, res) => registerController.register(req, res),
+  );
 
 module.exports = register;
