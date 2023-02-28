46394862a540dc594b11362d6aaad03c51d51b05
Miguel Inácio <miguel.angelo.inacio@gmail.com>
2023-02-28 17:48:11 -0300

    modifica role padrão para customer e adiciona id ao retorno

--- back-end/src/controllers/registerController.js
+++ back-end/src/controllers/registerController.js
@@ -2,7 +2,7 @@ const registerService = require('../services/registerService');
 
 const register = async (req, res) => {
   const data = req.body;
-  const result = await registerService.register(data);
+  const result = await registerService.register({ ...data, role: 'customer' });
 
   return res.status(201).json(result);
 };
--- back-end/src/services/registerService.js
+++ back-end/src/services/registerService.js
@@ -6,6 +6,7 @@ const register = async (data) => {
   const hashedPassword = md5(password);
   const result = await User.create({ name, email, password: hashedPassword, role });
   const user = {
+    id: result.id,
     name: result.name,
     email: result.email,
     role: result.role,
