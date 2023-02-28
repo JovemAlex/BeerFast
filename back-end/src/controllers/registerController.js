54a625d35bafdd1e258e6aff30d8b1811e5e1963
Miguel Inácio <miguel.angelo.inacio@gmail.com>
2023-02-28 15:09:54 -0300

    adiciona método register

---
+++ back-end/src/controllers/registerController.js
@@ -0,0 +1,11 @@
+const registerService = require('../services/registerService');
+
+
+const register = async (req, res) => {
+  const data = req.body;
+  const result = await registerService.register(data);
+
+  return res.status(201).json(result);
+}
+
+module.exports = { register }
