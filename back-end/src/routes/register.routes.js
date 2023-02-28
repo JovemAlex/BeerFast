9fbb13e869ce64eb0ef9e187dbcf9575e297efe1
Miguel Inácio <miguel.angelo.inacio@gmail.com>
2023-02-28 19:27:57 -0300

    refatora rota

--- back-end/src/routes/register.routes.js
+++ back-end/src/routes/register.routes.js
@@ -1,15 +1,13 @@
 const { Router } = require('express');
 const registerController = require('../controllers/registerController');
-const { verifyFields } = require('../middlewares/verifyFields');
-const { verifyName, verifyPassword } = require('../middlewares/verifyLength');
+const { verifyFields, verifyRegisterData } = require('../middlewares/verifyFields');
 
 const register = Router();
 
 register.post(
   '/',
   verifyFields,
-  verifyName, 
-  verifyPassword, 
+  verifyRegisterData,
   (req, res) => registerController.register(req, res),
   );
 
