fb17b73b3b5a0184f759403048cc9093ac47d5ef
Aline Valadao <valadao.aline@gmail.com>
2023-03-01 18:01:17 +0100

    feat: products endpoint (getAllProducts and static images)

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const loginRouter = require('../routes/loginRouter');
-
+const productRouter = require('../routes/productsRouter');
 const app = express();
-app.use(express.json());
 
+app.use(express.json());
+app.use('/images', express.static(__dirname + '/images'));
 app.get('/coffee', (_req, res) => res.status(418).end());
 app.use('/login', loginRouter);
+app.use('/products', productRouter);
 
 module.exports = app;
---
+++ back-end/src/controllers/productController.js
@@ -0,0 +1,8 @@
+const productService = require('../services/productService');
+
+const getAllProducts = async (_req, res) => {
+  const products = await productService.getAllProducts();
+  return res.status(200).json(products);
+}
+
+module.exports = { getAllProducts };
