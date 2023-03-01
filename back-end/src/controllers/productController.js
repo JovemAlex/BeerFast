53c1e5f5fa56fe20550ce66d4585dc56b901c3a5
Miguel Inácio <miguel.angelo.inacio@gmail.com>
2023-03-01 15:29:21 -0300

    fix: lint

--- back-end/src/controllers/productController.js
+++ back-end/src/controllers/productController.js
@@ -4,7 +4,6 @@ const getAllProducts = async (_req, res, next) => {
   try {
     const products = await productService.getAllProducts();
     return res.status(200).json(products);
-    
   } catch (err) {
     next(err);
   }
