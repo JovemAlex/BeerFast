bffe2e5f34dd9d11193e1f9023cb132cb4912842
Aline Valadao <valadao.aline@gmail.com>
2023-03-01 19:09:47 +0100

    fix: productController

--- back-end/src/controllers/productController.js
+++ back-end/src/controllers/productController.js
@@ -1,8 +1,13 @@
 const productService = require('../services/productService');
 
-const getAllProducts = async (_req, res) => {
-  const products = await productService.getAllProducts();
-  return res.status(200).json(products);
+const getAllProducts = async (_req, res, next) => {
+  try {
+    const products = await productService.getAllProducts();
+    return res.status(200).json(products);
+    
+  } catch (err) {
+    next(err);
+  }
 };
 
 module.exports = { getAllProducts };
