6b1eadb85818a790a06caaad9d7e4b8160e3cb86
Alberto Fernandes Couto <62117863+betim009@users.noreply.github.com>
2023-03-02 18:39:37 -0300

    Update App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Switch>
         <Route exact path="/login" component={ Login } />
         <Route exact path="/register" component={ Register } />
-        <Route exact path="/" render={ () => <Redirect to="/login" /> } />  
+        <Route exact path="/" render={ () => <Redirect to="/login" /> } />
       </Switch>
     </AppProvider>
   );
