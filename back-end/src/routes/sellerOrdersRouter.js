dda9460997fd9c184f45b19d0aaf7e51b6062cf6
Miguel Inácio <miguel.angelo.inacio@gmail.com>
2023-03-02 17:49:39 -0300

    fix: corrige linter

--- back-end/src/routes/sellerOrdersRouter.js
+++ back-end/src/routes/sellerOrdersRouter.js
@@ -4,11 +4,31 @@ const verifyToken = require('../middlewares/verifyToken');
 
 const sellerOrdersRouter = express.Router();
 
-sellerOrdersRouter.put('/:id/status/pendente', verifyToken, sellerOrdersController.updateStatusPendente);
-sellerOrdersRouter.put('/:id/status/preparando', verifyToken, sellerOrdersController.updateStatusPreparando);
-sellerOrdersRouter.put('/:id/status/em-transito', verifyToken, sellerOrdersController.updateStatusEmTransito);
-sellerOrdersRouter.get('/:id', verifyToken, sellerOrdersController.getOrderById);
+sellerOrdersRouter.put(
+  '/:id/status/pendente',
+  verifyToken,
+  sellerOrdersController.updateStatusPendente
+  );
+sellerOrdersRouter.put(
+  '/:id/status/preparando',
+  verifyToken,
+  sellerOrdersController.updateStatusPreparando
+  );
+sellerOrdersRouter.put(
+  '/:id/status/em-transito',
+  verifyToken,
+  sellerOrdersController.updateStatusEmTransito
+  );
+sellerOrdersRouter.get(
+  '/:id',
+  verifyToken,
+  sellerOrdersController.getOrderById
+  );
 
-sellerOrdersRouter.get('/', sellerOrdersController.getAllOrdersBySeller);
+sellerOrdersRouter.get(
+  '/',
+  verifyToken,
+  sellerOrdersController.getAllOrdersBySeller
+  );
 
 module.exports = sellerOrdersRouter;
