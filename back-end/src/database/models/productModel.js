1bda91f4346a71c3dddf9a4fe235e4d59dff48cc
Miguel Inácio <miguel.angelo.inacio@gmail.com>
2023-03-03 19:05:22 -0300

    fix: corrige nomes de model de snake_case para camelCase

--- back-end/src/database/models/productModel.js
+++ back-end/src/database/models/productModel.js
@@ -7,17 +7,17 @@ module.exports = (sequelize, DataTypes) => {
     },
     name: DataTypes.STRING,
     price: DataTypes.NUMBER,
-    url_image: DataTypes.STRING,
+    urlImage: DataTypes.STRING,
   },
   {
     timestamps: false,
-    underscored: false,
+    underscored: true,
     tableName: 'products',
   }
   );
 
   Product.associate = (models) => {
-    Product.hasMany(models.SaleProduct, { foreignKey: 'product_id', as: 'productId' });
+    Product.hasMany(models.SaleProduct, { foreignKey: 'productId', as: 'product' });
   };
 
   return Product;
