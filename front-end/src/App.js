877bedf6664785d03a9d2005817c00eaae1c81f8
Miguel Inácio <miguel.angelo.inacio@gmail.com>
2023-03-08 13:26:47 -0300

    feat: adiciona rota /orders/id para seller e customer

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -16,14 +16,13 @@ function App() {
         <Route exact path="/login" component={ Login } />
         <Route exact path="/register" component={ Register } />
         <Route exact path="/" render={ () => <Redirect to="/login" /> } />
-        <Route exact path="/seller/orders/:id" component={ Orders } />
+        <Route exact path="/seller/orders/:id" component={ OrderDetails } />
         <Route exact path="/seller/orders" component={ Orders } />
         <Route exact path="/customer/checkout" component={ Checkout } />
         <Route exact path="/customer/products" component={ Products } />
         <Route exact path="/admin/manage" component={ Admin } />
         <Route exact path="/customer/orders" component={ Orders } />
         <Route exact path="/customer/orders/:id" component={ OrderDetails } />
-
       </Switch>
     </AppProvider>
   );
