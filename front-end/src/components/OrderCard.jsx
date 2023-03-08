97d5fd8a21391d1c552e5a028bdf94e8c582c35e
Miguel Inácio <miguel.angelo.inacio@gmail.com>
2023-03-08 13:31:14 -0300

    fix: correções e validação de role

--- front-end/src/components/OrderCard.jsx
+++ front-end/src/components/OrderCard.jsx
@@ -1,16 +1,17 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useHistory } from 'react-router-dom';
 import AppContext from '../contexts/AppContext';
 
 export default function OrderCard(props) {
   const { orderDetails } = props;
-  const role = useContext(AppContext);
+  const { role } = useContext(AppContext);
   const [showAddress, setShowAddress] = useState(false);
   const history = useHistory();
+  const userRole = role || JSON.parse(localStorage.getItem('user')).role;
 
   const redirectToDetails = (id) => {
-    history.push(`${role}/orders/${id}`);
+    history.push(`/${userRole}/orders/${id}`);
   };
 
   useEffect(() => {
@@ -23,18 +24,18 @@ export default function OrderCard(props) {
         { `id do pedido: ${orderDetails.id}` }
       </p>
       <p data-testid={ `${role}_orders__element_card_price` }>
-        { `preço total: ${orderDetails.total_price}` }
+        { `preço total: ${orderDetails.totalPrice}` }
       </p>
       { showAddress
        && (
          <p
            data-testid="seller_orders__element_card_address"
          >
-           { `${orderDetails.delivery_address}, ${orderDetails.delivery_number}` }
+           { `${orderDetails.deliveryAddress}, ${orderDetails.deliveryNumber}` }
          </p>
        )}
       <p data-testid={ `${role}_orders__element_order_date` }>
-        { `data da venda: ${orderDetails.sale_date}` }
+        { `data da venda: ${orderDetails.saleDate}` }
       </p>
       <p data-testid={ `${role}_orders__element_delivery_status` }>
         { `status: ${orderDetails.status}` }
@@ -46,12 +47,12 @@ export default function OrderCard(props) {
 OrderCard.propTypes = {
   orderDetails: PropTypes.shape({
     id: PropTypes.number.isRequired,
-    user_id: PropTypes.number.isRequired,
-    seller_id: PropTypes.number.isRequired,
-    total_price: PropTypes.string.isRequired,
-    delivery_address: PropTypes.string.isRequired,
-    delivery_number: PropTypes.string.isRequired,
-    sale_date: PropTypes.string.isRequired,
+    userId: PropTypes.number.isRequired,
+    sellerId: PropTypes.number.isRequired,
+    totalPrice: PropTypes.string.isRequired,
+    deliveryAddress: PropTypes.string.isRequired,
+    deliveryNumber: PropTypes.string.isRequired,
+    saleDate: PropTypes.string.isRequired,
     status: PropTypes.string.isRequired,
   }).isRequired,
 };
